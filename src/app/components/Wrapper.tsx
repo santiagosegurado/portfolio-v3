09fe196fb2b458eb47a047931ff0c6135ff9b73c
Santiago <santisegurado1@gmail.com>
2024-12-04 02:15:10 -0300

    Add Footer component to Wrapper to improve page structure

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Navbar from "./navbar/Navbar";
 import ResponsiveNavbar from "./navbar/ResponsiveNavbar";
 import ResponsiveNavList from "./navbar/ResponsiveNavList";
+import Footer from "./Footer";
 
 type WrapperProps = {
   children: React.ReactNode;
@@ -16,10 +17,9 @@ const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   };
 
   return (
-    <section className="max-w-full bg-primaryBlue h-[calc(100vh-50px)] lg:h-[calc(100vh-110px)] m-6 lg:m-14 rounded-xl border border-accentBorder font-[family-name:var(--font-fira-code)] relative overflow-hidden">
+    <section className="max-w-full bg-primaryBlue h-[calc(100vh-50px)] lg:h-[calc(100vh-110px)] m-6 lg:m-14 rounded-xl border border-accentBorder font-[family-name:var(--font-fira-code)] relative 2xl:max-h-[1200px] 2xl:max-w-[1700px] 2xl:mx-auto flex  flex-col justify-between overflow-hidden">
       <Navbar />
       <ResponsiveNavbar
-        menuModalToggle={menuModalToggle}
         handleMenuModalToggle={handleMenuModalToggle}
       />
       <ResponsiveNavList
@@ -27,6 +27,7 @@ const Wrapper: React.FC<WrapperProps> = ({ children }) => {
         handleMenuModalToggle={handleMenuModalToggle}
       />
       {children}
+      <Footer />
     </section>
   );
 };
