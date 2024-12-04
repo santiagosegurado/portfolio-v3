2bb91de508b78844bddc72093adb7fd55ddf6cff
Santiago <santisegurado1@gmail.com>
2024-12-04 20:45:04 -0300

    Add new SVG icons: ArrowIcon, CloseIcon, FileIcon, FolderIcon and JSIcon

---
+++ src/app/components/icons/ArrowIcon.tsx
@@ -0,0 +1,12 @@
+const ArrowIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M12 16L6 10H18L12 16Z"></path>
+  </svg>
+);
+
+export default ArrowIcon;
