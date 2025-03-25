ceca402394f14dd4336fcd5d22a3eafe22f41fec
Santiago <santisegurado1@gmail.com>
2025-03-24 22:55:21 -0300

    Añade modelos y texturas 3D, y mejora la carga de la interfaz con un componente Canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import Button from "./components/ui/Button";
 import Link from "next/link";
 import GitHubIcon from "./components/icons/GitHubIcon";
+import { Canvas } from "@react-three/fiber";
+import { Bounds, Cloud, Clouds, PerspectiveCamera } from "@react-three/drei";
+import { Tamagochi } from "./components/3d-models/Tamagochi";
+import { MeshLambertMaterial } from "three";
 
 export default function Home() {
   const [isCopy, setIsCopy] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const customStyle = {
     background: "none",
     padding: "0",
   };
+
+  const interpolateFunc = (t: number) => 1 - Math.exp(-5 * t) + 0.007 * t;
 
   const code = `
   // You can copy my email by pressing the button on the side 
@@ -27,6 +34,15 @@ export default function Home() {
       setIsCopy(false);
     }, 3000);
   };
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <main className="w-full flex items-center justify-center gap-10 relative">
@@ -68,7 +84,7 @@ export default function Home() {
               )}
             </button>
           </div>
-          <div className="flex flex-row gap-3">
+          <div className="flex flex-row gap-3 relative">
             <Button style="primary">
               <GitHubIcon className="w-5 h-5" />
               <Link
@@ -95,6 +111,40 @@ export default function Home() {
                 linkedin
               </Link>
             </Button>
+            {/* 3d Object */}
+            <section className="w-72 h-60 absolute right-0 hidden md:flex">
+              {isLoading ? (
+                <div className="flex items-center justify-center h-full">
+                  <span className="text-secondaryWhite">Loading...</span>
+                </div>
+              ) : (
+                <Canvas className="h-full w-full">
+                  <Bounds
+                    fit
+                    clip
+                    observe
+                    margin={1.2}
+                    maxDuration={1}
+                    interpolateFunc={interpolateFunc}
+                  >
+                    <PerspectiveCamera makeDefault position={[0, 12, 50]} />
+                    <ambientLight intensity={1} />
+                    <directionalLight position={[10, 10, 5]} intensity={1} />
+                    <pointLight position={[90, 10, 10]} />
+                    <Tamagochi />
+                    <Clouds material={MeshLambertMaterial}>
+                      <Cloud
+                        seed={1}
+                        scale={2}
+                        volume={5}
+                        color="#607b96"
+                        fade={100}
+                      />
+                    </Clouds>
+                  </Bounds>
+                </Canvas>
+              )}
+            </section>
           </div>
         </div>
       </section>
